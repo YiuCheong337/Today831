b19ebd37e2b345585df3b3aa5300931c1df6a29c
YC Cheng <follienbr1@gmail.com>
2022-12-31 12:23:25 -0500

    ok to commit

--- WSP007-Learning/exercise/public/index.js
+++ WSP007-Learning/exercise/public/index.js
@@ -1,22 +1,22 @@
 // Change the selector to select your memo input form
 document
-    .querySelector('#memo-form')
-    .addEventListener('submit', async (event)=>{
+    .querySelector("#memo-form")
+    .addEventListener("submit", async (event)=>{
 		event.preventDefault(); // To prevent the form from submitting synchronously
 			     
         const form = event.target;
 		
         //... create your form object with the form inputs
         let formObject = {
-            text: form.text.value
+            text: form.text.value,
         };
 			     
-		const res = await fetch('/memos',{
+		const res = await fetch('/memos', {
             method:"POST",
 			headers:{
-				"Content-Type":"application/json"
+				"Content-Type":"application/json",
             },
-            body: JSON.stringify(formObject)
+            body: JSON.stringify(formObject),
 		});
 
 		// Clear the form here
