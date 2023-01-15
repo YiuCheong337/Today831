e770c58d6bbdcd935a33b95068b67075cf628f5b
YC Cheng <follienbr1@gmail.com>
2023-01-15 13:16:37 -0500

    before 2b

--- WSP009-Learning/exercise/app.ts
+++ WSP009-Learning/exercise/app.ts
@@ -1,10 +1,10 @@
 import {Client} from 'pg';
 import dotenv from 'dotenv';
-// import XLSX from XLSX;
+import XLSX from 'xlsx';
 
 dotenv.config();
 
-const client = new Client({
+export const client = new Client({
     database: process.env.DB_NAME,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
@@ -13,6 +13,11 @@ const client = new Client({
 // console.log("process.env.DB_NAME", client.database);
 // console.log("process.env.DB_USERNAME", client.user);
 // console.log("process.env.DB_PASSWORD", client.password);
+
+interface User{
+    username:string;
+    password: string;
+}
 
 async function main() {
     await client.connect() // "dial-in" to the postgres server
@@ -23,10 +28,20 @@ async function main() {
       password: 'offuck2',
     }
 
-    await client.query(
-      'INSERT INTO users (username,password) values ($1,$2)',
-      [user.username,user.password]
-    )
+    // https://docs.sheetjs.com/docs/api/parse-options/
+    const workbook = XLSX.readFile("my-workbook.xlsx").Sheets["user"];
+
+    // XLSX.readFile(fileName).Sheets[sheetName] !== undefined;
+
+    console.log(workbook);
+
+    // https://docs.sheetjs.com/docs/api/utilities/
+    // const userList: User[] = XLSX.utils.sheet_to_json(userWorksheet);
+
+    // await client.query(
+    //   'INSERT INTO users (username,password) values ($1,$2)',
+    //   [user.username,user.password]
+    // )
   
     // const result = await client.query(
     //   'SELECT * from users where username = $1',
